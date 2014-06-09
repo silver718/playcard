e6049266cf1836f934576d3df56ad2f02f3dffc9
Pei Qian <pqian@peis-imac.cn.ibm.com>
2014-06-09 17:41:07 +0800

    add new class Card

---
+++ playcard/Card.h
@@ -0,0 +1,15 @@
+//
+//  Card.h
+//  playcard
+//
+//  Created by Pei Qian on 6/9/14.
+//  Copyright (c) 2014 Pei Qian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@property (strong) NSString *contents;
+
+@end
---
+++ playcard/Card.m
@@ -0,0 +1,17 @@
+//
+//  Card.m
+//  playcard
+//
+//  Created by Pei Qian on 6/9/14.
+//  Copyright (c) 2014 Pei Qian. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface Card()
+
+@end
+
+@implementation Card
+
+@end
