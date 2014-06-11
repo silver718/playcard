3b62ec6cec1129977e8d755eeadea636c7661d2a
Pei Qian <pqian@Peis-iMac.local>
2014-06-11 16:50:40 +0800

    add class PlayingCardDeck

---
+++ playcard/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  playcard
+//
+//  Created by Pei Qian on 6/11/14.
+//  Copyright (c) 2014 Pei Qian. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
